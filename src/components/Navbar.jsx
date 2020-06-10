0fc16e52cf9ac6e2d0fd235ade5577236612d227
cmrivera <cmrivera0004@student.stcc.edu>
2020-06-09 21:51:45 -0400

    working on darkmode

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
+import DarkModeToggle from "react-dark-mode-toggle";
+import { darkTheme, lightMode } from "../hooks/useDarkMode";
 
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(false);
-  const toggleMode = e => {
+  const [darkMode, setDarkMode] = useState("light");
+  const [darkMode, togglearkMode] = darkTheme;
+  const toggleMode = (e) => {
     e.preventDefault();
-    setDarkMode(!darkMode);
+    if (darkMode === "light") {
+      window.localStorage.setItem("darkMode", "dark");
+      setDarkMode("dark");
+      //otherwise make lightMode
+    } else {
+      window.localStorage.setItem("darkMode", "light");
+      setDarkMode("light");
+    }
   };
+
+  useEffect(() => {
+    const localMode = window.localStorage.getItem("darkMode");
+    if (localMode) {
+      setDarkMode(localMode);
+    }
+  }, []);
+
   return (
     <nav className="navbar">
       <h1>Crypto Tracker</h1>
       <div className="dark-mode__toggle">
         <div
           onClick={toggleMode}
-          className={darkMode ? 'toggle toggled' : 'toggle'}
+          className={darkMode ? "toggle toggled" : "toggle"}
         />
       </div>
     </nav>
--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,12 +1,18 @@
-export const darkMode = {
-  body: "#363537",
-  text: "#FAFAfA",
-  toggleBorder: "#6B8096",
-  gradient: "linear-gradient(#091236, #1E215D)",
-};
-export const lightMode = {
-  body: "#E2E2E2",
-  text: "#363537",
-  toggleBorder: "#FFF",
-  gradient: "linear-gradient(#39598A, #79D7ED",
+import { useEffect } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
+
+export const useDarkMode = (key) => {
+  const [darkMode, setDarkMode] = useLocalStorage(key);
+
+  useEffect(() => {
+    return darkMode === false
+      ? () => {
+          document.body.className = "dark-mode";
+        }
+      : () => {
+          document.body.className = null;
+        };
+  }, [darkMode]);
+
+  return [darkMode, setDarkMode];
 };
